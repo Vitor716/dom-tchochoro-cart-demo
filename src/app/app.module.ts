5a963bd6b8db18e9f61df3f804e4cf4c92c41170
Vitor Vieira <vitorvieira_deoliveira@hotmail.com>
2023-05-16 12:43:42 -0300

    header, all produducts, products and header folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { ProductSelectedComponent } from './all-products/product-selected/product-selected.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AllProductsComponent,
+    ProductSelectedComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
